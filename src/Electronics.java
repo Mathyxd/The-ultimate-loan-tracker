97f8f12d83d515ce9f0fa2c7b3a389fab27d7481
Marcus <mali1004@stud.ek.dk>
2026-02-19 11:17:36 +0100

    tt

--- src/Electronics.java
+++ src/Electronics.java
@@ -1,2 +1,22 @@
-public class Electronics {
+public abstract class Electronics extends LoanItem{
+    private double baseCost;
+
+    public Electronics(String title, int loanDays, double baseCost)  {
+        super(title, loanDays);
+        this.baseCost = baseCost;
+    }
+
+    public double getBaseCost(){
+        return baseCost;
+    }
+
+    public double calculateLateFees() {
+        return baseCost * 0.20;
+    }
+
+    public String toString() {
+        return "Title: " + getTitle() + "Loan Days: " + getLoanDays() + "Late fee per day: " + calculateLateFees() + " kr";
+        //super.toString();
+    }
+
 }
