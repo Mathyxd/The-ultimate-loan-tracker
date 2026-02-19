fab081b1b3ad5cdf946e75b2ca6811404df7e459
Rasmusfp <rap01001@stud.ek.dk>
2026-02-19 10:20:06 +0100

    Superklasse

--- src/LoanItem.java
+++ src/LoanItem.java
@@ -1,2 +1,34 @@
-public class LoanItem {
+public abstract class LoanItem {
+
+    private String title;
+    private int loanDays;
+
+    //Constructor
+    public Loanitem(String title, int loanDays) {
+        this.title = title;
+        this.loanDays = loanDays;
+    }
+
+    //Getter
+    public String getTitle() {
+    return title;
+    }
+
+    public int getLoanDays() {
+    return loanDays;
+    }
+
+    //Setter
+    public void setTitle(String title) {
+    this.title = title;
+    }
+
+    public void setLoanDays(int loanDays) {
+    this.loanDays = loanDays;
+    }
+
+    //Subklasser implementeres
+    public abstract double calculateLateFees();
+
+    public abstract String getDescription();
 }
